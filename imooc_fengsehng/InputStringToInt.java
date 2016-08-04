d386c5699b8306a15db23860cbb1b4a16d240c29
yang-cc <yang_cc@hotmail.com>
2016-08-05 01:52:02 +0800

    commit InputStringtoInt.java

---
+++ imooc_fengsehng/InputStringToInt.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("plz input a Integer:");
+		Scanner in = new Scanner(System.in);
+		String inStr = in.nextLine();
+		int outInt = Solution.myAtoi(inStr);
+		System.out.println("String:\"" + inStr + "\" --> Int:" + outInt);
+	}
+	public static class Solution {
+		public static int myAtoi(String str) {
+			//首先判断空值
+			if(str == null){
+				return 0;
+			}
+			//去掉空格的情况
+			str = str.trim();
+			if(str.length() == 0){
+				return 0;
+			}
+			int sign = 1;
+			int index = 0;
+			if(str.charAt(index) == '+'){
+				index++;
+			}else if(str.charAt(index) == '-'){
+				index++;
+				sign = -1;
+			}
+			//取得数字部分，遇到溢出和非数字退出
+			long number = 0;
+			System.out.println("---index:string---");
+			for(;index < str.length();index++){
+				if(str.charAt(index) < '0' || str.charAt(index) > '9'){
+					break;
+				}
+				System.out.println(index + ":" + str.charAt(index));
+				number = number *10 + (str.charAt(index) - '0');
+				System.out.println("number:" + number);
+				if(number >= Integer.MAX_VALUE){
+					break;
+				}
+			}
+			if(number * sign >= Integer.MAX_VALUE){
+				return Integer.MAX_VALUE;
+			}
+			if(number * sign <= Integer.MIN_VALUE){
+				return Integer.MIN_VALUE;
+			}
+			return (int)number*sign;
+		}
+	}
+}
