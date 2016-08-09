22fce0cd9c846ac53f2dc97696e4c947578ad258
yang-cc <yang_cc@hotmail.com>
2016-08-09 10:45:03 +0800

    modify "+" to "|"

--- imooc_fengsehng/_03_SumOfTwoInt.java
+++ imooc_fengsehng/_03_SumOfTwoInt.java
@@ -28,7 +28,7 @@ public class _03_SumOfTwoInt {
         int r = 0, c = 0, recipe = 1;
         while ((a | b | c) != 0) {
             if (((a ^ b ^ c) & 1) != 0)
-                r += recipe;
+                r |= recipe;
             recipe <<= 1;
             c = (a & b | b & c | a & c) & 1;
             a >>>= 1;
